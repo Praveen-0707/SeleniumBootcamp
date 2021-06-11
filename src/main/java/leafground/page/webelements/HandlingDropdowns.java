bb4a7b699e96d191b04aee2c502eafd1bfaad834
aRRaj <aRRaj@LAPTOP-4GCDTFLO>
2021-06-11 23:07:02 +0530

    added Selectable code

--- src/main/java/leafground/page/webelements/HandlingDropdowns.java
+++ src/main/java/leafground/page/webelements/HandlingDropdowns.java
@@ -1,16 +1,20 @@
 package leafground.page.webelements;
 
+import java.awt.event.KeyEvent;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class HandlingDropdowns {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		WebDriverManager.chromedriver().setup();
 		ChromeDriver driver = new ChromeDriver();
@@ -54,6 +58,25 @@ public class HandlingDropdowns {
 		selectMultipleOptions.selectByIndex(1);
 		selectMultipleOptions.selectByVisibleText("UFT/QTP");
 		selectMultipleOptions.selectByValue("4");
+		
+		driver.navigate().back();
+		Thread.sleep(2000);
+		
+//		Handling Selectable Listbox
+		
+//		Clicks on Image Selectable
+		WebElement imgSelectable = driver.findElementByXPath("//h5[text()='Selectable']/following-sibling::img");
+		imgSelectable.click();
+		
+		List<WebElement> lists = driver.findElementsByXPath("//ol[@id='selectable']/li");
+		System.out.println(lists);
+		
+		Actions action = new Actions(driver);
+		for (int i=0; i<3; i++)
+		{
+			action.keyDown(Keys.CONTROL).click(lists.get(i)).perform();
+		}
+		
 	}
 
 }
